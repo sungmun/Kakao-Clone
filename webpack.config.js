95d33e2e70975d909313daec430600915eb86ead
seungmun <tjdans174@gmail.com>
2018-11-10 22:41:58 +0900

    Fead : webpack update에 따른 설정 변경
    기본값 생략

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports={
-    entry:'./src/index.js',
-    output:{
-        path:__dirname+'/public',
-        filename:'bundle.js'
-    },
-    module:{
-        loaders:[
+    mode: 'development',
+    /* 이 부분은 entry와 output의 기본값으로 생략 가능합니다.
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    }, */
+    module: {
+        rules: [
             {
-                test:/\.js$/,
-                loader: 'babel',
-                exclude:/node_modules/,
-                query:{
-                    cacheDirectory:true,
-                    presets:['es2015','react']
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                   loader: 'babel-loader'
                 }
             }
         ]
