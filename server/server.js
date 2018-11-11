9a72098270a79106e5bf9cb3d9f432515d992b7f
seungmun <tjdans174@gmail.com>
2018-11-11 22:48:15 +0900

    Fead : WebPack적용하여 서버 구동 부분 변경

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import router from './router/main';
 import OAuth from './auth/oauth';
 
 const app = express();
+const port =3000;
+const devPort=3001;
+if(process.env.NODE_ENV==='development'){
+    console.log('Server is running on development mode');
+
+    const config = require('../webpack.config');
+    let compiler = webpack(config);
+    let devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(devPort, () => {
+        console.log('webpack-dev-server is listening on port', devPort);
+    });
+}
 
 app.set('views',__dirname+'/views');
 app.set('view engine','ejs');
@@ -26,6 +40,6 @@ db.once('open',()=>console.log('Connected to Mongodb Server'));
 
 mongoose.connect('mongodb://localhost/kakao_talk',{useNewUrlParser:true});
 
-const server=app.listen(3000,()=>console.log('Express Start'));
+const server=app.listen(port,()=>console.log('Express listening on port', port));
 
 //supervisor server.js
