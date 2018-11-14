71c4d29861b10ea7da3fd121822385bb790f4b3a
seungmun <tjdans174@gmail.com>
2018-11-14 19:45:54 +0900

    Fead : .babelrc - >  babel.config.js
    상세한 설정 파일로 변경

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+const presets=[
+    [
+        "@babel/env",
+        {
+            targets:{
+                edge:"17",
+                firefox:"60",
+                chrome:"67",
+                safari:"11.1"
+            },
+            useBuiltIns:"usage",
+        },
+    ],
+    [
+        "@babel/preset-react",
+        {
+            targets:{
+                edge:"17",
+                firefox:"60",
+                chrome:"67",
+                safari:"11.1"
+            },
+            useBuiltIns:"usage",
+        }
+    ]
+];
+
+module.exports={presets};
