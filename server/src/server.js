e658ce12fcfd2ba23db84eb461506a9f657f47a1
seungmun <tjdans174@gmail.com>
2018-11-18 19:07:05 +0900

    webpack관련 설정은 전부 제거

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,12 @@
 import express from 'express';
-import WebpackDevServer from 'webpack-dev-server';
-import webpack from 'webpack';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import bodyParser from'body-parser';
+import mongoose from'mongoose';
 import router from './router/main';
 import OAuth from './auth/oauth';
 
 const app = express();
 const port =3000;
 const devPort=3001;
-if(process.env.NODE_ENV==='development'){
-    console.log('Server is running on development mode');
-
-    const config = require('../webpack.config');
-    let compiler = webpack(config);
-    let devServer = new WebpackDevServer(compiler, config.devServer);
-    devServer.listen(devPort, () => {
-        console.log('webpack-dev-server is listening on port', devPort);
     });
 }
 
