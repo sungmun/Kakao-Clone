02ea28e4b637e2ccfdeddabf721a523ef22df0ca
seungmun <tjdans174@gmail.com>
2018-11-18 19:10:50 +0900

    Next관련 설정 적용및 실행

--- server/src/server.js
+++ server/src/server.js
@@ -3,33 +3,44 @@ import bodyParser from'body-parser';
 import mongoose from'mongoose';
 import router from './router/main';
 import OAuth from './auth/oauth';
+import next from 'next';
+const dev = process.env.NODE_ENV !== 'production'
 
-const app = express();
-const port =3000;
-const devPort=3001;
+const app = next({dev});
+
+const handle=app.getRequestHandler();
+
+const port =3001;
+
+app.prepare()
+    .then(()=>{
+        const server=express();
+        server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({extended:true}));
+        
+        server.use('/api',router);
+
+         
+        var oauth=new OAuth(server);
+        server.use('/api/auth',oauth.googleOAuth())
+
+        // server.get('*',Auth,(req,res)=>handle(req,res));
+        server.get('*',(req,res,next)=>{
+            if(req.isAuthenticated()) {
+                return app.render(req, res);
+            }
+            return app.render(req, res, '/login');
+        });
+        const db=mongoose.connection;
+        db.on('error',console.error);
+        db.once('open',()=>console.log('Connected to Mongodb Server'));
+        
+        mongoose.connect('mongodb://localhost/kakao_talk',{useNewUrlParser:true});
+        
+        server.listen(port,(err)=>console.log('Express listening on port', port));
     });
-}
 
-app.set('views',__dirname+'/views');
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
 
-app.use(express.static('public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use('/',router);
-
-var oauth=new OAuth(app);
-app.use('/auth',oauth.googleOAuth())
-
-const db=mongoose.connection;
-db.on('error',console.error);
-db.once('open',()=>console.log('Connected to Mongodb Server'));
-
-mongoose.connect('mongodb://localhost/kakao_talk',{useNewUrlParser:true});
-
-const server=app.listen(port,()=>console.log('Express listening on port', port));
 
 //supervisor server.js
