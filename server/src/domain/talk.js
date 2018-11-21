f81c30cbb28a6cc2b718a1a325bd2e27ccd25177
seungmun <tjdans174@gmail.com>
2018-11-21 16:58:03 +0900

    Fix: Clean Code
    SChema 클래스만 불러오도록 변경

--- server/src/domain/talk.js
+++ server/src/domain/talk.js
@@ -1,12 +1,10 @@
-import mongoose from 'mongoose';
+import { Schema } from "mongoose";
 
-var Schema=mongoose.Schema;
-
-var talkSchema=new Schema({
-    from:{type:Schema.Types.ObjectId, ref:'member'},
-    to:{type:Schema.Types.ObjectId, ref:'member'},
-    message:String,
-    time:{type:Date,default:Date.now}
+const talkSchema = new Schema({
+    from: { type: Schema.Types.ObjectId, ref: "member" },
+    to: { type: Schema.Types.ObjectId, ref: "member" },
+    message: String,
+    time: { type: Date, default: Date.now }
 });
 
-export default mongoose.model('talk',talkSchema);
+export default mongoose.model("talk", talkSchema);
