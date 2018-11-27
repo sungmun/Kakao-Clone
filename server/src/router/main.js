6647e182393794962cbdc765ca09df2b29eff1fd
seungmun <tjdans174@gmail.com>
2018-11-27 18:22:38 +0900

    Fix : 기존에 있던 라우터 모듈 제거

--- server/src/router/main.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router=express.Router();
-
-router.get('/',(req,res)=>res.send('/'));
-
-router.get('/about',(req,res)=>res.render('/about'));
-
-export default router;
