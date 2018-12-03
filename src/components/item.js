9cf5de15ab518c5db9a712e6386d94181cdf8937
seungmun <tjdans174@gmail.com>
2018-12-03 19:29:06 +0900

    [#3] [#4] Fead : TalkRoom이나 profile에 사용이 될 item 컴포넌트

---
+++ src/components/item.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export default ({ image, children }) => (
+    <Item>
+        <ItemImage src={image} />
+        <ItemContent>{children}</ItemContent>
+    </Item>
+);
+
+const Item = styled.li`
+    display: flex;
+    margin: 5px 15px;
+    height: 60px;
+`;
+const ItemImage = styled.img`
+    border-radius: 50%;
+    margin: 10px;
+    width: 40px;
+    flex-direction: row;
+`;
+
+const ItemContent = styled.div`
+    flex-direction: row;
+    width: 100%;
+    height: 60px;
+`;
