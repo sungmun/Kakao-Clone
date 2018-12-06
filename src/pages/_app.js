33282e69983f590fc128d246cf4e2187218a1ca5
seungmun <tjdans174@gmail.com>
2018-12-06 18:33:50 +0900

    [#10] add LoginAjax Function
    유저의 데이터를 얻어오기위해 ajax모듈을 추가 했으며, 로그인후 유저의 데이터를 저장해놓기 위해 store와 action을 추가했다.
    그리고 islogin함수가 필요가 없어 제거하였다

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import App, { Container } from "next/app";
 import React from "react";
-import initstore from "../store";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
+import axios from "axios";
+
+import initstore from "../store";
 import { getToken } from "../utils/auth";
+import { login } from "../actions/user";
 
 class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
@@ -16,13 +19,23 @@ class MyApp extends App {
         };
     }
 
+    LoginAjax = (token, successCall) => {
+        axios
+            .get("/user", { params: { token } })
+            .then(res => successCall(res.data.info))
+            .catch(err => console.error(err));
+    };
+
     isToken = token => (token === "" || token == undefined ? false : true);
 
     componentDidMount() {
         if (window.location.href === "/login") return;
 
         const token = getToken();
-        if (!this.islogin(token)) Router.push({ pathname: "/login" });
+        if (!this.isToken(token)) Router.push({ pathname: "/login" });
+
+        const store = this.props.store;
+        this.LoginAjax(token, user => store.dispatch(login(user)));
     }
 
     render() {
