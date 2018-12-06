20e82832fcbb7a348f1819fe3b1c0ff65a092d15
seungmun <tjdans174@gmail.com>
2018-12-06 18:23:49 +0900

    [#10] add getToken function
    현제 페이지가 login페이지가 login페이지가 아닐시 토큰의 값을 호출하는 함수를 사용한다
    
    이후 토큰값이 있으면 서버에 토큰값을 보내고 사용자의 정보를 불러오는 과정을 진행할 것이고, 없을경우 로그인페이지로 이동할 것이다.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import React from "react";
 import initstore from "../store";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
+import { getToken } from "../utils/auth";
+
 class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
         return {
@@ -13,8 +15,15 @@ class MyApp extends App {
             }
         };
     }
+    componentDidMount() {
+        if (window.location.href === "/login") return;
+
+        const token = getToken();
+    }
+
     render() {
         const { Component, pageProps, store } = this.props;
+
         return (
             <Container>
                 <Provider store={store}>
