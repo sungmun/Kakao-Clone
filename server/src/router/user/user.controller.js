9cb2210161ebec97dc0a8c0633ef9f884ba9a06f
seungmun <tjdans174@gmail.com>
2018-12-08 17:39:51 +0900

    Simplification check function
     - check함수에서 검사를 하는 부분을 제외
     - 토큰에 들어가는 데이터 축소
     - 토큰을 발급함수 간략화
     - 함수 이름 변경

--- server/src/router/user/user.controller.js
+++ server/src/router/user/user.controller.js
@@ -5,26 +5,7 @@ exports.login = (req, res, next) => {
     const { platforName, socialId, nickName, photos } = req.body.user;
     const secret = req.app.get("jwt-secret");
 
-    const check = member => {
-        if (!member) throw Error("login failed");
-        const p = new Promise((resolve, reject) => {
-            jwt.sign(
-                {
-                    id: member.id,
-                    nickName: member.nickName,
-                    photos: member.photos,
-                    platforName: member.platforName
-                },
-                secret,
-                { expiresIn: "7d", issuer: "veloper.com", subject: "user" },
-                (err, token) => {
-                    if (err) reject(err);
-                    resolve(token);
-                }
-            );
-        });
-        return p;
-    };
+    const tokenIssue = member => jwt.sign({ id: member._id }, secret);
 
     const respond = token => {
         res.status(201);
