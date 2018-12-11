cb515e11f53e0cf7927d77d9220d810e3db4c61f
seungmun <tjdans174@gmail.com>
2018-12-11 19:54:49 +0900

    Modified reducers/user.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -12,10 +12,10 @@ export default (state = initalState, action) => {
         case LOGIN:
             return {
                 ...state,
-                platforName: action.platforName,
-                socialId: action.socialId,
-                nickName: action.nickName,
-                photos: action.photos
+                platforName: action.profile.platforName,
+                socialId: action.profile.socialId,
+                nickName: action.profile.nickName,
+                photos: action.profile.photos
             };
         case CHANGE_NICKNAME:
             return {
