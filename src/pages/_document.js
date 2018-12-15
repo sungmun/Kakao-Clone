926f6619297e2263ec7bcb5ca7e16173fc247f74
seungmun <tjdans174@gmail.com>
2018-12-15 17:25:39 +0900

    Run CSS rendering on the server
    서버사이드에서 CSS Rendering을 하도록 변경

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,23 @@
 import Document, { Head, Main, NextScript } from "next/document";
-import Axios from "axios";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: App => props =>
+                    sheet.collectStyles(<App {...props} />)
+            });
+
         const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+        return {
+            ...initialProps,
+            styles: [initialProps.styles, ...sheet.getStyleElement()]
+        };
     }
 
     render() {
