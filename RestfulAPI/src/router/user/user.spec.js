93f4930b0873a120b6d0eb5d629c22dc8517929d
seungmun <tjdans174@gmail.com>
2018-12-29 13:35:19 +0900

    [#13] add TDD file
    /user Url로 오는 데이터에 대한 테스트 케이스를  작성하였다

---
+++ RestfulAPI/src/router/user/user.spec.js
@@ -0,0 +1,106 @@
+import request from "supertest";
+import chai, { expect } from "chai";
+import http from "chai-http";
+
+chai.use(http);
+
+const url = "http://localhost:5000";
+describe("User.Controller", () => {
+    const user = {
+        platforName: "google",
+        socialId: "tjdans174@gmail.com"
+    };
+
+    describe("User login Test", () => {
+        it("should return error", done => {
+            chai.request(url)
+                .post("/user")
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res.body).have.property("success");
+                    expect(res.body).have.property("message");
+
+                    expect(res.body.success).to.be.equal(false);
+                    done();
+                });
+        });
+        it("should return the token", done => {
+            chai.request(url)
+                .post("/user")
+                .send({ user })
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res.body).have.property("success");
+                    expect(res.body).have.property("message");
+
+                    expect(res.body.success).to.be.equal(true);
+                    expect(res.body.message).have.property("token");
+                    done();
+                });
+        });
+    });
+
+    describe("User login check Test", () => {
+        it("should return the not loggged in", done => {
+            chai.request(url)
+                .get("/user")
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res.body).have.property("success");
+                    expect(res.body).have.property("message");
+
+                    expect(res.body.success).to.be.equal(false);
+                    done();
+                });
+        });
+        it("should return err", done => {
+            chai.request(url)
+                .get("/user")
+                .set("x-access-token", "body")
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res.body).have.property("success");
+                    expect(res.body).have.property("message");
+                    expect(res.body.success).to.be.equal(false);
+                    done();
+                });
+        });
+        it("should return the profile", done => {
+            chai.request(url)
+                .post("/user")
+                .send({ user })
+                .end((err, res) => {
+                    expect(err).to.be.null;
+
+                    const body = res.body;
+                    expect(body).have.property("success");
+                    expect(body).have.property("message");
+
+                    expect(body.success).to.be.equal(true);
+                    expect(body.message).to.be.property("token");
+
+                    chai.request(url)
+                        .get("/user")
+                        .set("x-access-token", body.message.token)
+
+                        .end((err, res) => {
+                            expect(err).to.be.null;
+
+                            expect(res.status).to.equal(201);
+
+                            const body = res.body;
+                            expect(body).have.property("success");
+                            expect(body).have.property("message");
+
+                            const profile = expect(body.message.profile);
+                            profile.have.property("socialId");
+                            profile.have.property("platforName");
+                            profile.have.property("nickName");
+                            profile.have.property("photos");
+
+                            done();
+                        });
+                });
+        });
+    });
+});
