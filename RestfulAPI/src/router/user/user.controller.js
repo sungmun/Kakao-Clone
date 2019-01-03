bf9e8067b27dc293fc55e0ff70c9069c9704d118
seungmun <tjdans174@gmail.com>
2019-01-03 14:54:54 +0900

    [#16] Edit RestfulAPI user.controller.js
    user.constroller의 login함수내부에서 사용되는 mongoose를 변경하였다

--- RestfulAPI/src/router/user/user.controller.js
+++ RestfulAPI/src/router/user/user.controller.js
@@ -1,37 +1,28 @@
 import jwt from "jsonwebtoken";
-import Member from "../../domain/member";
+import Model from "../../models";
 import { secret } from "../../../private-key.json";
 const sendMessage = (success, message) => ({ success, message });
 
 exports.login = (req, res) => {
-    const tokenIssue = profile => jwt.sign({ id: profile._id }, secret);
-
-    const respond = token => {
-        res.status(201).json(sendMessage(true, { token }));
-    };
-
-    const OnError = error => {
-        res.status(403).json(sendMessage(false, error.message));
-    };
-
-    const register = member => {
-        new Member(member).save();
-        return member;
-    };
-
     const reqCheak = new Promise((resolve, reject) => {
         req.body.user || reject(Error("유저 정보가 없습니다"));
         resolve(req.body.user);
     });
 
-    const memberfind = profile => {
-        return (
-            Member.findOne({
-                platforName: profile.platforName,
+    const tokenIssue = profile => jwt.sign({ id: profile.id }, secret);
+
+    const memberfind = profile =>
+        Model.Members.findOne({
+            where: {
+                platformName: profile.platformName,
                 socialId: profile.socialId
-            }) || register(profile)
-        );
-    };
+            }
+        }).then(result => result || Model.Members.create(profile));
+
+    const respond = token => res.status(201).json(sendMessage(true, { token }));
+
+    const OnError = error =>
+        res.status(403).json(sendMessage(false, error.message));
 
     reqCheak
         .then(memberfind)
