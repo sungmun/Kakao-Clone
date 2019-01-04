359021360a0348f9f9757f50e9041c0a9ca03054
seungmun <tjdans174@gmail.com>
2019-01-04 17:32:58 +0900

    [#13] Add Restful function
    add getToken, resCheack, ErrorProcess function
    - getToken : token을 받아오는 프로미스를 반환한다.
    - resCheack : res를 받아 지정한 형식에 맞는 body인지 체크한다.
    - ErrorProcess : Error가 발생할 경우 Error를 처리하는 로직을 통일하여 준다

--- RestfulAPI/src/router/user/user.spec.js
+++ RestfulAPI/src/router/user/user.spec.js
@@ -4,12 +4,34 @@ import http from "chai-http";
 chai.use(http);
 
 const url = "http://localhost:5000";
-describe("User.Controller", () => {
-    const user = {
-        platformName: "google",
-        socialId: "tjdans174@gmail.com"
-    };
 
+const user = {
+    platformName: "google",
+    socialId: "tjdans174@gmail.com"
+};
+
+const ErrorProcess = err => console.log(err);
+
+const resCheack = res => {
+    expect(res.body).have.property("success");
+    expect(res.body).have.property("message");
+    return res;
+};
+
+const getToken = new Promise((resolve, reject) => {
+    chai.request(url)
+        .post("/user")
+        .send({ user })
+        .then(resCheack)
+        .then(res => {
+            expect(res.body.success).to.be.equal(true);
+            expect(res.body.message).have.property("token");
+            return res.body.message.token;
+        })
+        .then(resolve)
+        .catch(reject);
+});
+describe("User.Controller", () => {
     describe("User login Test", () => {
         it("should return error", done => {
             chai.request(url)
