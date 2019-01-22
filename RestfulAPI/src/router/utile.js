9b154666708488ba004b207621f1b096501c6e26
seungmun <tjdans174@gmail.com>
2019-01-22 17:30:13 +0900

    [#23] Code refactoring RestfulAPI utile
    모든 동작이 끝나면 next를 실행하며, cheack함수의 동작을 실행하기 위한 라이브러리를 추가 하였다.

--- RestfulAPI/src/router/utile.js
+++ RestfulAPI/src/router/utile.js
@@ -1,4 +1,7 @@
-exports.check = (req, res, next) => {
+import { secret } from "../../private-key.json";
+import jwt from "jsonwebtoken";
+import Model from "../database/models";
+export const cheack = (req, res, next) => {
     const token = req.headers["x-access-token"] || req.query.token;
 
     const promiss = new Promise((resolve, reject) => {
@@ -9,17 +12,17 @@ exports.check = (req, res, next) => {
         );
     });
 
-    const respond = profile => {
-        req.body.profile = profile;
-    };
+    const respond = profile => (req.body.profile = profile);
 
     const onError = error =>
         res.status(403).json(sendMessage(false, error.message));
 
     promiss
         .then(profile => Model.Members.findByPk(profile.id))
+        .then(profile => profile.dataValues)
         .then(respond)
-        .catch(onError);
+        .catch(onError)
+        .then(next);
 };
 
 export const messageFormat = (success, message) => ({ success, message });
