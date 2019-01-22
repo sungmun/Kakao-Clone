c5609973356ca8c36f13449eb7205d437a749cb3
seungmun <tjdans174@gmail.com>
2019-01-22 23:36:06 +0900

    [#21] Edite RestfulAPI friend.controller
    friend controller에 있는 함수를 전부 next인자를 추가함으로서 Middleware로 변경하였다

--- RestfulAPI/src/router/friend/friend.controller.js
+++ RestfulAPI/src/router/friend/friend.controller.js
@@ -1,14 +1,17 @@
 import Model from "../../database/models";
 import { messageFormat } from "../utile";
 
-export const read = (req, res) => {
+export const read = (req, res, next) => {
     res.status(201).json(messageFormat(true, {}));
+    next();
 };
 
-export const save = (req, res) => {
+export const save = (req, res, next) => {
     res.status(201).json(messageFormat(true, {}));
+    next();
 };
 
-export const remove = (req, res) => {
+export const remove = (req, res, next) => {
     res.status(201).json(messageFormat(true, {}));
+    next();
 };
