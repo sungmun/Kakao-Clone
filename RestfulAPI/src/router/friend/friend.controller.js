b14df685622fa8433389b35a6f0268644dcac294
seungmun <tjdans174@gmail.com>
2019-01-23 21:49:47 +0900

    [#7] Edite friend.controller read function
    read 함수의 내용을 제거하고, req에서 현제 유저의 정보인 profile을 저장한다

--- RestfulAPI/src/router/friend/friend.controller.js
+++ RestfulAPI/src/router/friend/friend.controller.js
@@ -2,8 +2,8 @@ import Model from "../../database/models";
 import { messageFormat } from "../utile";
 
 export const read = (req, res, next) => {
-    res.status(201).json(messageFormat(true, {}));
-    next();
+    const user = req.body.profile;
+
 };
 
 export const save = (req, res, next) => {
