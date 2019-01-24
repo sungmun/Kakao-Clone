053667b936ea392b9621c2ddfdb9053c12f847b0
seungmun <tjdans174@gmail.com>
2019-01-24 15:54:22 +0900

    [#7] Edite friend.controller save function
    기존의 베이스 코드 제거

--- RestfulAPI/src/router/friend/friend.controller.js
+++ RestfulAPI/src/router/friend/friend.controller.js
@@ -31,8 +31,6 @@ export const read = (req, res, next) => {
 };
 
 export const save = (req, res, next) => {
-    res.status(201).json(messageFormat(true, {}));
-    next();
 };
 
 export const remove = (req, res, next) => {
