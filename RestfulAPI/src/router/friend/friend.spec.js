194b3cdb731fa8bf5d8b97d182ced65fb050e36c
seungmun <tjdans174@gmail.com>
2019-01-25 16:28:58 +0900

    [#21] Edite friend Test Case
    Mocks객체를 만들어주는 함수를 이용하여 token값을 추가한 Mocks객체를 반환해주는 함수 생성

--- RestfulAPI/src/router/friend/friend.spec.js
+++ RestfulAPI/src/router/friend/friend.spec.js
@@ -4,6 +4,12 @@ import { auth, convertMiddlewareToPromise } from "../utile";
 import { login } from "../user/user.controller";
 import { remove, save, read } from "./friend.controller";
 
+const setTokenMocks = (method, data, token) => {
+    const { req, res } = setMocks(method, data);
+    req.headers = { "x-access-token": token };
+    return { req, res };
+};
+
 const setMocks = (method, data) =>
     createMocks({
         method: method,
