a8ecdf2b3c30ae388d4f8cf3a581edb088dabc8b
seungmun <tjdans174@gmail.com>
2019-01-30 19:09:03 +0900

    [#8] Add talkRoom.controller file
    총 7개의 함수를 선언해준다.
    - save : talkRoom을 만들어 주는 함수이다. 인자값으로는 초기 초대 유저의 정보를 배열로 받는다
    - read : talkRoom의 내용물을 출력해주는 함수로, 채팅방에 들어와았는 유저의 정보와 대화정보를 전부 출력해준다
    - listRead : 현재 로그인한 user의 talkRoom리스트를 반환해주는 함수
    - remove : 유저가 talkRoom에서 나갈때 동작을 하는 함수
    - addUser : talkRoom에 유저가 추가될때 시행되는 함수

---
+++ RestfulAPI/src/router/talk-room/talkRoom.controller.js
@@ -0,0 +1,32 @@
+//talkRoom생성
+export const save = (req, res, next) => {
+    next();
+};
+
+//talkRoom내용 보기
+export const read = (req, res, next) => {
+    next();
+};
+//talkRoom리스트보기
+export const listRead = (req, res, next) => {
+    next();
+};
+
+//talkRoom 변경
+export const update = (req, res, next) => {
+    next();
+};
+
+//talkRoom 마지막 남은 유저 나가기
+export const remove = (req, res, next) => {
+    next();
+};
+
+//talkRoom 유저추가
+export const addUser = (req, res, next) => {
+    next();
+};
+//talkRoom 유저제거
+export const removeUser = (req, res, next) => {
+    next();
+};
