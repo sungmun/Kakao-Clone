a6691ffc811dd8831aef52b63d9cbe5c248f288b
seungmun <tjdans174@gmail.com>
2019-01-30 18:14:38 +0900

    [#8] Edite talkRoom model script
    talkRoom table을 User테이블을 n:m관계로 연결을 해주고, 이때 외래키로 talkId를 만들도록 한다

--- RestfulAPI/src/database/models/talkRoom.js
+++ RestfulAPI/src/database/models/talkRoom.js
@@ -1,6 +1,12 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
     const TalkRoom = sequelize.define("TalkRoom", {}, {});
-    TalkRoom.associate = function(models) {};
+    TalkRoom.associate = function(models) {
+        TalkRoom.belongsToMany(models.User, {
+            as: "UserList",
+            through: "UserTalkRooms",
+            foreignKey: "talkId"
+        });
+    };
     return TalkRoom;
 };
