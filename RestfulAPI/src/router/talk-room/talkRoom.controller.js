bf5145f603cbfef74218cb28a0747bc27dde9b94
sungmun <tjdans174@gmail.com>
2019-02-06 20:04:29 +0900

    Test :  Add talkRoom.controller read function content for testing
    read 함수에 간단한 테스트를 진행하기 위해 test용 내용 추가
    
    Resolves: #8
    See also: #23

--- RestfulAPI/src/router/talk-room/talkRoom.controller.js
+++ RestfulAPI/src/router/talk-room/talkRoom.controller.js
@@ -7,7 +7,13 @@ export const save = (req, res, next) => {
 
 //talkRoom내용 보기
 export const read = (req, res, next) => {
-    next();
+    const OnError = ({ message }) => res.status(403).json(message);
+
+    new Promise((resolve, reject) => {
+        reject(Error("내용이 없음"));
+    })
+        .catch(OnError)
+        .finally(next);
 };
 //talkRoom리스트보기
 export const listRead = (req, res, next) => {
