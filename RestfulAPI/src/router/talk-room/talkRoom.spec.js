864b655942f7f8e4961d8c8f05f01010de9e9b24
sungmun <tjdans174@gmail.com>
2019-02-11 16:19:04 +0900

    test : Edite controller test case
    모듈화된 테스트 코드를 불러오는 부분으로, 기존에는 로그인이 성공을 하고, 반환된 토큰값을 전달을 해주는 방식이였으나, 유통기한이 긴 JWT를 만들어서 private json파일에 저장을 하고 모듈마다 불러오는 방식으로 수정을 하였다
    
    Resolves: #8

--- RestfulAPI/src/router/talk-room/talkRoom.spec.js
+++ RestfulAPI/src/router/talk-room/talkRoom.spec.js
@@ -1,38 +1,15 @@
-import { createMocks } from "node-mocks-http";
-import { TestCaseUtile } from "../utile";
-import { login } from "../user/user.controller";
+import TestCase from './testCase';
 
-import { TestCase } from "./testCase";
+describe('talkRoom.Controller', () => {
+    describe('save', () => TestCase.save());
 
-describe("talkRoom.Controller", () => {
-    let token;
+    describe('addUser', () => TestCase.addUser());
 
-    before(() =>
-        TestCaseUtile.convertMiddlewareToPromise(
-            login,
-            createMocks({
-                method: "POST",
-                body: {
-                    user: {
-                        platformName: "google",
-                        socialId: "tjdans174@gmail.com"
-                    }
-                }
-            })
-        )
-            .then(TestCaseUtile.getData)
-            .then(data => (token = data.message.token))
-    );
+    describe('listRead', () => TestCase.listRead());
 
-    describe("addUser", () => TestCase.addUser(token));
+    describe('read', () => TestCase.read());
 
-    describe("listRead", () => TestCase.listRead(token));
+    describe('remove', () => TestCase.remove());
 
-    describe("read", () => TestCase.read(token));
-
-    describe("remove", () => TestCase.remove(token));
-
-    describe("save", () => TestCase.save(token));
-
-    describe("update", () => TestCase.update(token));
+    describe('update', () => TestCase.update());
 });
