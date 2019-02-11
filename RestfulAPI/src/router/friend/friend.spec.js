9f50e7a21da4cf54810994ff0560bc659ba41438
sungmun <tjdans174@gmail.com>
2019-02-11 21:50:06 +0900

    test : refactor friend test code
    - change airbnb style
    - remove messageFormat
    - add Token
    
    Resolves: #21
    See also : #23,#24

--- RestfulAPI/src/router/friend/friend.spec.js
+++ RestfulAPI/src/router/friend/friend.spec.js
@@ -1,124 +1,78 @@
-import { expect } from "chai";
-import { createMocks } from "node-mocks-http";
-import { auth, convertMiddlewareToPromise } from "../utile";
-import { login } from "../user/user.controller";
-import { remove, save, read } from "./friend.controller";
+/* eslint-disable no-undef */
+import { expect } from 'chai';
+import { auth, TestCaseUtile } from '../utile';
+import { newToken } from '../../../private-key.json';
+import { remove, save, read } from './friend.controller';
 
-const setTokenMocks = (method, data, token) => {
-    const { req, res } = setMocks(method, data);
-    req.headers = { "x-access-token": token };
-    return { req, res };
-};
+const { convertMiddlewareToPromise, setTokenMocks, getData } = TestCaseUtile;
 
-const setMocks = (method, data) =>
-    createMocks({
-        method: method,
-        body: data
-    });
-
-describe("friend.Controller", () => {
-    let token;
-
-    before(() =>
-        convertMiddlewareToPromise(
-            login,
-            setMocks("POST", {
-                user: {
-                    platformName: "google",
-                    socialId: "tjdans174@gmail.com"
-                }
-            })
-        ).then(({ res }) => (token = JSON.parse(res._getData()).message.token))
-    );
-
-    describe("save", () => {
-        let data;
+describe('friend.Controller', () => {
+    describe('save', () => {
+        let code;
         before(() =>
             convertMiddlewareToPromise(
                 auth,
-                setTokenMocks("POST", { friend: 2 }, token)
+                setTokenMocks('POST', { friend: 2 }, newToken)
             )
                 .then(promiseData =>
                     convertMiddlewareToPromise(save, promiseData)
                 )
-                .then(({ res }) => (data = JSON.parse(res._getData())))
+                .then(promiseData => {
+                    code = promiseData.res.statusCode;
+                })
         );
 
-        it("message type cheack", () =>
-            expect(data).to.have.all.keys("success", "message"));
-
-        it("should return success", () =>
-            expect(data.success).to.be.equal(true));
-
-        it("should friend cheack", () =>
-            expect(data.message).to.have.key("friend"));
-
-        it("should friend type cheack", () =>
-            expect(data.message.friend).to.have.all.keys(
-                "createdAt",
-                "id",
-                "friendId",
-                "userId",
-                "updatedAt"
-            ));
+        it('should return statusCode 204', () =>
+            expect(code).to.be.equals(204));
     });
 
-    describe("read", () => {
+    describe('read', () => {
         let data;
-        before(() =>
-            convertMiddlewareToPromise(auth, setTokenMocks("get", null, token))
-                .then(promiseData =>
-                    convertMiddlewareToPromise(read, promiseData)
-                )
-                .then(({ res }) => (data = JSON.parse(res._getData())))
-        );
-
-        it("message type cheack", () =>
-            expect(data).to.have.all.keys("success", "message"));
-
-        it("should return success", () =>
-            expect(data.success).to.be.equal(true));
-
-        it("should return Array", () =>
-            expect(data.message.friend).to.be.an("array"));
-
-        it("should return Array type profile", () =>
-            data.message.friend.forEach(profile =>
-                expect(profile).to.have.all.keys(
-                    "id",
-                    "createdAt",
-                    "updatedAt",
-                    "socialId",
-                    "platformName",
-                    "nickName",
-                    "photos"
-                )
-            ));
-    });
-
-    describe("remove", () => {
-        let data, status;
         before(() =>
             convertMiddlewareToPromise(
                 auth,
-                setTokenMocks("delete", { friend: 2 }, token)
+                setTokenMocks('get', null, newToken)
+            )
+                .then(promiseData =>
+                    convertMiddlewareToPromise(read, promiseData)
+                )
+                .then(promiseData => {
+                    data = getData(promiseData);
+                })
+        );
+
+        it('should return Array', () => expect(data.friend).to.be.an('array'));
+
+        it('should return Array type profile', () =>
+            data.friend.forEach(profile =>
+                expect(profile).to.have.all.keys(
+                    'id',
+                    'createdAt',
+                    'updatedAt',
+                    'socialId',
+                    'platformName',
+                    'nickName',
+                    'photos'
+                )
+            ));
+    });
+
+    describe('remove', () => {
+        let code;
+        before(() =>
+            convertMiddlewareToPromise(
+                auth,
+                setTokenMocks('delete', { friend: 2 }, newToken)
             )
                 .then(promiseData =>
                     convertMiddlewareToPromise(remove, promiseData)
                 )
-                .then(({ res }) => {
-                    status = res.status;
-                    data = JSON.parse(res._getData());
+                .then(promiseData => {
+                    code = promiseData.res.statusCode;
                 })
         );
 
-        it("message type cheack", () =>
-            expect(data).to.have.all.keys("success", "message"));
-
-        it("should return success", () =>
-            expect(data.success).to.be.equal(true));
-
-        it("should verify that the removed value is 1", () =>
-            expect(data.message.row).to.be.equal(1));
+        it('should return statusCode 204', () =>
+            expect(code).to.be.equals(204));
     });
 });
