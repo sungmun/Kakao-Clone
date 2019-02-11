dbdd9cb37d869ebceb5633bba3def65052229687
sungmun <tjdans174@gmail.com>
2019-02-11 21:44:13 +0900

    style : change to airbnb style
    Resolves: #8
    See also : #24

--- RestfulAPI/src/router/talk-room/talkRoom.controller.js
+++ RestfulAPI/src/router/talk-room/talkRoom.controller.js
@@ -1,13 +1,12 @@
-import Model from "../../database/models";
+import Model from '../../database/models';
 
-//talkRoom생성
+// talkRoom생성
 export const save = (req, res, next) => {
-    const user = req.body.profile;
-    const friends = req.body.friends;
+    const { profile: user, friends } = req.body;
 
     const friendsCheack = new Promise((resolve, reject) => {
-        if (friends === undefined) reject(Error("friends 값이 없습니다"));
-        if (!Array.isArray(friends)) reject(Error("friends 배열이 아닙니다"));
+        if (friends === undefined) reject(Error('friends 값이 없습니다'));
+        if (!Array.isArray(friends)) reject(Error('friends 배열이 아닙니다'));
         resolve(friends);
     });
 
@@ -25,8 +24,8 @@ export const save = (req, res, next) => {
 
     friendsCheack
         .then(userBuild)
-        .then(user => user.getFriendList())
-        .then(data => data || Error("데이터가 없습니다"))
+        .then(userData => userData.getFriendList())
+        .then(data => data || Error('데이터가 없습니다'))
         .then(friendList =>
             friendList.map(friend => friend.get({ plain: true }))
         )
@@ -38,54 +37,54 @@ export const save = (req, res, next) => {
         .finally(next);
 };
 
-//talkRoom내용 보기
+// talkRoom내용 보기
 export const read = (req, res, next) => {
     const OnError = ({ message }) => res.status(403).json(message);
 
     new Promise((resolve, reject) => {
-        reject(Error("내용이 없음"));
+        reject(Error('내용이 없음'));
     })
         .catch(OnError)
         .finally(next);
 };
-//talkRoom리스트보기
+// talkRoom리스트보기
 export const listRead = (req, res, next) => {
     const OnError = ({ message }) => res.status(403).json(message);
 
     new Promise((resolve, reject) => {
-        reject(Error("내용이 없음"));
+        reject(Error('내용이 없음'));
     })
         .catch(OnError)
         .finally(next);
 };
 
-//talkRoom 변경
+// talkRoom 변경
 export const updata = (req, res, next) => {
     const OnError = ({ message }) => res.status(403).json(message);
 
     new Promise((resolve, reject) => {
-        reject(Error("내용이 없음"));
+        reject(Error('내용이 없음'));
     })
         .catch(OnError)
         .finally(next);
 };
 
-//talkRoom 마지막 남은 유저 나가기
+// talkRoom 마지막 남은 유저 나가기
 export const remove = (req, res, next) => {
     const OnError = ({ message }) => res.status(403).json(message);
 
     const promise = new Promise((resolve, reject) => {
-        reject(Error("내용이 없음"));
+        reject(Error('내용이 없음'));
         resolve();
     });
 
     promise.catch(OnError).finally(next);
 };
 
-//talkRoom 유저추가
+// talkRoom 유저추가
 export const addUser = (req, res, next) => {
     const promisetest = new Promise((resolve, reject) => {
-        reject(Error("내용이 없음"));
+        reject(Error('내용이 없음'));
         resolve();
     });
 
