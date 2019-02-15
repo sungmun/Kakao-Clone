aa56aa78e6804e1224c0892cec9a7d923991e3e9
sungmun <tjdans174@gmail.com>
2019-02-15 23:31:32 +0900

    Test: detach friend testcase database
    database에 직접적으로 영향이 가는 함수를 sinon을 이용하여 가짜함수로 대체
    
    Resolves: #21

--- RestfulAPI/src/router/friend/friend.spec.js
+++ RestfulAPI/src/router/friend/friend.spec.js
@@ -1,10 +1,29 @@
 /* eslint-disable no-undef */
 import { expect } from 'chai';
+import { stub } from 'sinon';
 import { auth, TestCaseUtile } from '../utile';
 import { newToken } from '../../../private-key.json';
 import { remove, save, read } from './friend.controller';
+import Model from '../../database/models';
 
 const { convertMiddlewareToPromise, setTokenMocks, getData } = TestCaseUtile;
+
+stub(Model.Friend, 'findOrCreate').returns({
+    spread: fn =>
+        Promise.resolve(
+            fn({
+                get: () => ({
+                    id: 1,
+                    userId: 1,
+                    friendId: 2,
+                    createdAt: '2019-02-15 08:01:16',
+                    updatedAt: '2019-02-15 08:01:16'
+                })
+            })
+        )
+});
+
+stub(Model.Friend, 'destroy').resolves({ get: { row: 1 } });
 
 describe('friend.Controller', () => {
     describe('save', () => {
