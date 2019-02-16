ae8bffefe099f3bd062de52282eb68a63c003303
sungmun <tjdans174@gmail.com>
2019-02-16 17:31:46 +0900

    Test: detach talkroom adduser testcase database
    database에 직접적으로 영향이 가는 함수를 sinon을 이용하여 가짜함수로 대체
    
    Resolves: #20

--- RestfulAPI/src/router/talk-room/testCase/addUser.spec.js
+++ RestfulAPI/src/router/talk-room/testCase/addUser.spec.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-undef */
 import { expect } from 'chai';
+import { stub, restore } from 'sinon';
+import { newToken } from '../../../../private-key.json';
 import { auth, TestCaseUtile } from '../../utile';
 import { addUser } from '../talkRoom.controller';
-
-import { newToken } from '../../../../private-key.json';
+import Model from '../../../database/models';
 
 const { convertMiddlewareToPromise, getData, setTokenMocks } = TestCaseUtile;
 
 export default () => {
+    before(() => {
+        stub(Model.TalkRoom, 'build').callsFake(() => ({
+            reload: () => Promise.resolve({ addUserList: user => user })
+        }));
+    });
+
+    after(() => restore());
+
     describe('return success', () => {
         let data;
 
         before(() =>
             convertMiddlewareToPromise(
                 auth,
-                setTokenMocks('POST', { friend: 3 }, newToken)
+                setTokenMocks('POST', { friend: 3, talkroom: 1 }, newToken)
             )
                 .then(promiseData =>
                     convertMiddlewareToPromise(addUser, promiseData)
@@ -24,7 +33,7 @@ export default () => {
                 })
         );
 
-        it('should null cheack', () => expect(data).to.have.null);
+        it('should null cheack', () => expect(data).to.be.ok);
     });
 
     describe('argument null', () => {
@@ -40,6 +49,9 @@ export default () => {
                     )
                     .then(promiseData => {
                         data = getData(promiseData);
+                    })
+                    .catch(err => {
+                        data = err.message;
                     })
             );
 
