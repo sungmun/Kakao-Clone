1d6ebe9085fce336aa3ec16257211efcaa083907
sungmun <tjdans174@gmail.com>
2019-02-19 18:21:39 +0900

    Refactor : detach Seed data
    seed Data를 분리하고 외부에 노출을 함으로서 테스트시  mock를 만들때 사용이 가능하게 한다.
    
    See also : #20, #21, #22

--- RestfulAPI/src/database/seeders/20190109075425-profile.js
+++ RestfulAPI/src/database/seeders/20190109075425-profile.js
@@ -1,56 +1,39 @@
+const seedData = [
+    {
+        id: 1,
+        platformName: 'google',
+        socialId: 'tjdans174@gmail.com',
+        nickName: '강성문',
+        photos:
+            'https://lh3.googleusercontent.com/-CM57SJbKGEE/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcaiCCOhELETUdml3bdnYYf56nIv3A/s96-c/photo.jpg',
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        id: 2,
+        platformName: 'facebook',
+        socialId: 'tjdans174@naver.com',
+        nickName: '강성문',
+        photos:
+            'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=1088024871359658&height=50&width=50&ext=1549607711&hash=AeQq1fMUlwIYRahP',
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        id: 3,
+        platformName: 'google',
+        socialId: 'dkrak174@gmail.com',
+        nickName: '강성문',
+        photos:
+            'https://lh4.googleusercontent.com/-PfFszwf5seY/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcYyWHTOrGBE042hzQsvX4DkYwD_hg/s96-c/photo.jpg',
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    }
+];
 module.exports = {
+    seedData,
     up: queryInterface => {
-        return queryInterface.bulkInsert(
-            'Users',
-            [
-                {
-                    platformName: 'google',
-                    socialId: 'tjdans174@gmail.com',
-                    nickName: '강성문',
-                    photos:
-                        'https://lh3.googleusercontent.com/-CM57SJbKGEE/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcaiCCOhELETUdml3bdnYYf56nIv3A/s96-c/photo.jpg',
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    platformName: 'facebook',
-                    socialId: 'tjdans174@naver.com',
-                    nickName: '강성문',
-                    photos:
-                        'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=1088024871359658&height=50&width=50&ext=1549607711&hash=AeQq1fMUlwIYRahP',
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    platformName: 'google',
-                    socialId: 'dkrak174@gmail.com',
-                    nickName: '강성문',
-                    photos:
-                        'https://lh4.googleusercontent.com/-PfFszwf5seY/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcYyWHTOrGBE042hzQsvX4DkYwD_hg/s96-c/photo.jpg',
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                }
-            ],
-            {}
-        );
+        return queryInterface.bulkInsert('Users', seedData, {});
     },
 
     down: queryInterface => {
--- RestfulAPI/src/database/seeders/20190115065519-friend.js
+++ RestfulAPI/src/database/seeders/20190115065519-friend.js
@@ -1,71 +1,40 @@
+const seedData = [
+    {
+        userId: 1,
+        friendId: 2,
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        userId: 1,
+        friendId: 3,
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        userId: 2,
+        friendId: 1,
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        userId: 2,
+        friendId: 3,
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    },
+    {
+        userId: 3,
+        friendId: 1,
+        createdAt: '2019-02-16 12:28:00',
+        updatedAt: '2019-02-16 12:28:00'
+    }
+];
+
 module.exports = {
+    seedData,
     up: queryInterface => {
-        return queryInterface.bulkInsert(
-            'Friends',
-            [
-                {
-                    userId: 1,
-                    friendId: 2,
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    userId: 1,
-                    friendId: 3,
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    userId: 2,
-                    friendId: 1,
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    userId: 3,
-                    friendId: 1,
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                },
-                {
-                    userId: 2,
-                    friendId: 3,
-                    createdAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, ''),
-                    updatedAt: new Date()
-                        .toISOString()
-                        .replace(/T/, ' ')
-                        .replace(/\..+/, '')
-                }
-            ],
-            {}
-        );
+        return queryInterface.bulkInsert('Friends', seedData, {});
     },
 
     down: queryInterface => {
