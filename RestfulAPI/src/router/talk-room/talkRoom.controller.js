e9dd82f76025aabe117d766fe1a6e89d125a90b0
sungmun <tjdans174@gmail.com>
2019-02-20 16:24:18 +0900

    test : remove talkRoom.controller update function
    기존의 talkRoom에서 update부분이 현제로서는 필요가 없어 일단 제거후 차후에 필요성이 생기면 다시 만들것이다
    
    Resolves: #8

--- RestfulAPI/src/router/talk-room/talkRoom.controller.js
+++ RestfulAPI/src/router/talk-room/talkRoom.controller.js
@@ -104,17 +104,6 @@ export const listRead = (req, res, next) => {
         .finally(next);
 };
 
-// talkRoom 변경
-export const updata = (req, res, next) => {
-    const OnError = ({ message }) => res.status(403).json(message);
-
-    new Promise((resolve, reject) => {
-        reject(Error('내용이 없음'));
-    })
-        .catch(OnError)
-        .finally(next);
-};
-
 // talkRoom 마지막 남은 유저 나가기
 export const remove = (req, res, next) => {
     const { profile: user } = req.body;
