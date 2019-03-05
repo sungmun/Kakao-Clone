91dee62db113ffae3e682dcb9605b1374c6191fe
sungmun <tjdans174@gmail.com>
2019-03-05 18:35:56 +0900

    test : Edite talroom.controller remove testcase
    - removeFun을 통해 befor단축 및 쓸데 없는 부분 제거
    
    Resolves: #20

--- RestfulAPI/src/router/talk-room/testCase/remove.spec.js
+++ RestfulAPI/src/router/talk-room/testCase/remove.spec.js
@@ -16,23 +16,17 @@ const removeFun = (params, done) => {
 };
 
 export default () => {
+    before(() => {
+        stub(models.userTalkRooms, 'destroy').resolves({ row: 1 });
+    });
+
     describe('return success', () => {
         let code;
-        before(() =>
-            convertMiddlewareToPromise(
-                auth,
-                createMocks({
-                    method: 'DELETE',
-                    params: { talkRoom: 73 },
-                    headers: { 'x-access-token': newToken }
-                })
-            )
-                .then(promiseData =>
-                    convertMiddlewareToPromise(remove, promiseData)
-                )
-                .then(promiseData => {
-                    code = promiseData.res.statusCode;
-                })
+        before(done =>
+            removeFun({ talkRoom: 1 }, res => {
+                code = res.statusCode;
+                done();
+            })
         );
 
         it('should return statusCode 204', () =>
@@ -40,50 +34,17 @@ export default () => {
     });
 
     describe('return fail', () => {
-        describe('token null', () => {
-            let data;
-            before(() => {
-                convertMiddlewareToPromise(
-                    auth,
-                    createMocks({
-                        method: 'DELETE',
-                        params: 1
-                    })
-                )
-                    .then(promiseData =>
-                        convertMiddlewareToPromise(remove, promiseData)
-                    )
-                    .then(promiseData => {
-                        data = getData(promiseData);
-                    })
-                    .catch(error => {
-                        data = error.message;
-                    });
-            });
-
-            it('should return message ', () =>
-                expect(data).to.be.equal('not loggged in'));
-        });
-
         describe('params null', () => {
-            before(() => {
-                convertMiddlewareToPromise(
-                    auth,
-                    createMocks({
-                        method: 'DELETE',
-                        headers: { 'x-access-token': newToken }
-                    })
-                )
-                    .then(promiseData =>
-                        convertMiddlewareToPromise(remove, promiseData)
-                    )
-                    .then(promiseData => {
-                        data = getData(promiseData);
-                    });
+            let data;
+            before(done =>
+                removeFun(undefined, res => {
+                    data = getData({ res });
+                    done();
+                })
+            );
 
-                it('should return message', () =>
-                    expect(data).to.be.equal('params가 없습니다'));
-            });
+            it('should return message', () =>
+                expect(data).to.be.equal('params가 없습니다'));
         });
     });
 };
