292c890b32f48728077385258cc345f11b12b2ea
sungmun <tjdans174@gmail.com>
2019-03-31 17:51:20 +0900

    Fix : Edite Profile component js file
    기존의 방식에서 전에 만든 컴포넌트를 이용하는 방식으로 수정

--- client-side/src/component/Profile/index.js
+++ client-side/src/component/Profile/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import Item from 'component/Item';
+import Info from 'component/Info';
 import './app.scss';
 
-export default ({ image, nickName, intro }) => {
-    console.log(image);
-    console.log(nickName);
-    console.log(intro);
-    return (
-        <Item image={image}>
-            <div className="CenterBox">
-                <div className="Nickname">{nickName}</div>
-                <div>{intro}</div>
-            </div>
-        </Item>
-    );
-};
+export default ({ image, nickName, intro }) => (
+    <Item image={image}>
+        <Info nickName={nickName} email={intro} />
+    </Item>
+);
