d372353970854ace1a227d1e8c727d3d91ccf523
sungmun <tjdans174@gmail.com>
2019-04-01 21:18:33 +0900

    chore : remove proflie component scss file
    기존에 사용하는 컴포넌트를 사용하는 방식으로 변경함에 따라 scss파일이 필요가 없어져 제거

--- client-side/src/component/Profile/index.js
+++ client-side/src/component/Profile/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Item from 'component/Item';
 import Info from 'component/Info';
-import './app.scss';
 
 export default ({ image, nickName, intro }) => (
     <Item image={image}>
