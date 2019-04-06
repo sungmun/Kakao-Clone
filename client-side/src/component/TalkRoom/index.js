95f7f2cb05f67cf4f63b2c79233d2d531a57ed60
sungmun <tjdans174@gmail.com>
2019-04-06 19:38:41 +0900

    Test : Edite talkRoom component js file
    테스트를 위해 인자값으로 받는 값이 없을때 friends.json파일을 로드해서 사용하도록 수정

--- client-side/src/component/TalkRoom/index.js
+++ client-side/src/component/TalkRoom/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Item from 'component/Item';
+import friendJSON from 'component/TalkRoom/friend.json';
 
 const attributeFilter = (frineds = [], attr) => frineds.map(val => val[attr]);
 
-export default ({ friend = [] }) => {
+export default ({ friend = friendJSON }) => {
     return <Item image={attributeFilter(friend, 'photos')} />;
 };
