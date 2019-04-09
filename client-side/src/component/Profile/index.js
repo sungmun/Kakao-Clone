bfa3d8fa86e4b89dae084a8862469a3ead478527
sungmun <tjdans174@gmail.com>
2019-04-09 20:25:28 +0900

    Refactor : Edite image parameter
    기존의 파라미터중 image파라미터는 배열이 아닌 문자열을 무조건 받았으나, TalkRoom컴포넌트로 인해 image파라미터값이 배열형식으로 받도록 변경이 되어 약간의 수정을 가하였다

--- client-side/src/component/Profile/index.js
+++ client-side/src/component/Profile/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Item from 'component/Item';
 import Info from 'component/Info';
 
-export default ({ image, nickName, intro }) => (
-    <Item image={image}>
-        <Info nickName={nickName} email={intro} />
-    </Item>
-);
+export default ({ image, nickName, intro }) => {
+    const images = [image];
+    return (
+        <Item image={images}>
+            <Info nickName={nickName} email={intro} />
+        </Item>
+    );
+};
