72e3c9713d8406d176b24cd97491a890a5958e45
sungmun <tjdans174@gmail.com>
2019-04-10 20:31:20 +0900

    Fead: add nameView Function
    기존의 KakaoTalk은 상자의 크기에 따라 자연스럽게 표시가 되는 문자열의 길이가 틀렸으나, 현재 프로젝트에서는 상자의 크기가 고정이 되므로 문자열의 총 길이가 40이 넘어가는 경우 '...' 처리를 해주는 함수이다

--- client-side/src/component/TalkRoom/index.js
+++ client-side/src/component/TalkRoom/index.js
@@ -4,6 +4,16 @@ import friendJSON from 'component/TalkRoom/friend.json';
 
 const attributeFilter = (frineds = [], attr) => frineds.map(val => val[attr]);
 
+const nameView = (names = []) => {
+    const nameStr = names.join(', ');
+
+    if (nameStr.length > 40) {
+        return nameStr.slice(0, 39) + '...';
+    }
+
+    return nameStr;
+};
+
 export default ({ friend = friendJSON }) => {
     return <Item image={attributeFilter(friend, 'photos')} />;
 };
