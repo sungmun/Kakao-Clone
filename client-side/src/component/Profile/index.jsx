c1204caf898393549b8a73883143499b9dc4f73d
sungmun <tjdans174@gmail.com>
2019-04-27 18:26:29 +0900

    Style : change component/Profile/index.js to component/Profile/index.jsx
    js파일을 jsx파일로 변경을 하며 ESLint에 따라 propTypes를 설정해준다

---
+++ client-side/src/component/Profile/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shape, string, number } from 'prop-types';
+
+import Item from 'component/Item';
+import Info from 'component/Info';
+
+const Profile = ({ user }) => {
+  const { photos, nickName, id } = user;
+
+  return (
+    <Item image={new Array(photos)} url={`/user/${id}`}>
+      <Info nickName={nickName} />
+    </Item>
+  );
+};
+
+Profile.propTypes = {
+  user: shape({
+    photos: string.isRequired,
+    nickName: string.isRequired,
+    id: number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
