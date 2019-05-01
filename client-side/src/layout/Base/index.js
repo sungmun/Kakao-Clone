648cdcd5ca503dbfd448ac26f6d8d8e5a0bcc707
sungmun <tjdans174@gmail.com>
2019-05-01 19:18:54 +0900

    Style : change layout/Base/index.js to layout/Base/index.jsx
    js파일을 jsx파일로 변경을 하며 ESLint에 따라 propTypes를 설정해주었다

--- client-side/src/layout/Base/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './app.scss';
-
-export default ({ children }) => <div className="Container">{children}</div>;
---
+++ client-side/src/layout/Base/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { arrayOf, oneOfType, node } from 'prop-types';
+
+import './app.scss';
+
+const Base = ({ children }) => <div className="Container">{children}</div>;
+
+Base.propTypes = {
+  children: oneOfType([arrayOf(node), node]).isRequired,
+};
+
+export default Base;
