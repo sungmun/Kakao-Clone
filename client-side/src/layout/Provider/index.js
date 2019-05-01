ff91882ea7145a6284fdcc725601f3e00e1e1383
sungmun <tjdans174@gmail.com>
2019-05-01 19:20:32 +0900

    Style : change layout/Provider/index.js to layout/Provider/index.jsx
    js파일을 jsx파일로 변경을 하며 ESLint에 따라 propTypes를 설정해주었다

--- client-side/src/layout/Provider/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import reducer from 'reducer';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-export default ({ children }) => (
-    <Provider store={createStore(reducer)}>{children}</Provider>
-);
