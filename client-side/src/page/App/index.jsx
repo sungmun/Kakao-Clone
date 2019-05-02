3e4af1e77d80b562d97b75ec4c03fa9a52bdabad
sungmun <tjdans174@gmail.com>
2019-05-02 12:54:22 +0900

    Refactor: change page/App/index.js to page/App/index.jsx
    js파일을 jsx파일로 변경을 하며 ESLint에 따라 propTypes를 설정해주었다
    그리고 useRequest hooks부분을 함수를 이용하여 코드의 간소화를 하였다. 그외에도 tokenProp함수도 간략화를 하였다

---
+++ client-side/src/page/App/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Layout from 'layout/List/index';
+import Profile from 'component/Profile/index';
+import { axiosUseEffect, axiosErrorCatch } from 'event/hooks/useRequest/index';
+
+import './App.scss';
+
+import { connect } from 'react-redux';
+
+const index = ({ token }) => {
+  const factory = url => axiosUseEffect({ method: 'get', url }, { token });
+
+  const profileData = factory('/user');
+  const friendData = factory('/friend');
+
+  try {
+    const { profile } = axiosErrorCatch(profileData);
+    const { friend } = axiosErrorCatch(friendData);
+
+    return (
+      <Layout>
+        <ul className="List">
+          <span className="Profile">내 프로필</span>
+          <Profile user={profile} />
+          <hr className="Line" />
+          <span className="Profile">
+            친구
+            {` ${friend.length}`}
+          </span>
+          {friend.map(user => (
+            <Profile user={user} key={user.id} />
+          ))}
+        </ul>
+      </Layout>
+    );
+  } catch (e) {
+    return e.message === '/login' ? (
+      <Redirect to={e.message} />
+    ) : (
+      <div>{e.message}</div>
+    );
+  }
+};
+
+const tokenProps = ({ token }) => ({ token });
+
+export default connect(tokenProps)(index);
