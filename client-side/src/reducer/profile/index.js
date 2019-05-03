60e3b816f30c844523dae487d40b477a745f2484
sungmun <tjdans174@gmail.com>
2019-05-03 19:30:55 +0900

    Fix : add List a tag style
    리스트 목록에서 a태그를 추가함에 따라 하위 항목에 있던 flex속성이 사라지므로 다시 추가를 해주는 css

--- client-side/src/reducer/profile/index.js
+++ client-side/src/reducer/profile/index.js
@@ -1,16 +1,17 @@
 import { SET_PROFILE } from 'actions/profile';
+
 const initstate = {
-    platformName: '',
-    socialId: '',
-    nickName: '',
-    photos: ''
+  platformName: '',
+  socialId: '',
+  nickName: '',
+  photos: '',
 };
 
 export default (state = initstate, action) => {
-    switch (action.type) {
-        case SET_PROFILE:
-            return Object.assign({}, state, action.profile);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_PROFILE:
+      return Object.assign({}, state, action.profile);
+    default:
+      return state;
+  }
 };
