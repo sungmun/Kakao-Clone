679cd0b6877bb7c2318ff0faddf223fd326c82e9
sungmun <tjdans174@gmail.com>
2019-05-03 19:45:04 +0900

    Style : edite event/Axios function
    다이렉트로 return 하지 않고 변수에 담은후 리턴하도록 수정

--- client-side/src/event/Axios/index.js
+++ client-side/src/event/Axios/index.js
@@ -1,10 +1,11 @@
 import Axios from 'axios';
 
 export default async ({ method, url }, { token, data }) => {
-    return await Axios[method](`http://localhost:5000${url}`, {
-        ...data,
-        headers: {
-            'x-access-token': token
-        }
-    });
+  const res = await Axios[method](`http://localhost:5000${url}`, {
+    ...data,
+    headers: {
+      'x-access-token': token,
+    },
+  });
+  return res;
 };
