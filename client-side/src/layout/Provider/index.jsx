86dccfd0d28ae09d1eb92644e9a893ba917f671b
sungmun <tjdans174@gmail.com>
2019-05-08 18:49:09 +0900

    Fead : add redux middleware library
    기존의 root store에 redux-thunk middleware와 redux-devtools-extension middleware를 추가한다

--- client-side/src/layout/Provider/index.jsx
+++ client-side/src/layout/Provider/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { oneOfType, arrayOf, node } from 'prop-types';
 import reducer from 'reducer';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootStore = ({ children }) => (
-  <Provider store={createStore(reducer)}>{children}</Provider>
-);
+const rootStore = ({ children }) => {
+  const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+  );
+  return <Provider store={store}>{children}</Provider>;
+};
 
 rootStore.propTypes = {
   children: oneOfType([arrayOf(node), node]).isRequired,
