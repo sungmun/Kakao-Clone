75b0e00e96e8ab19fcdcbfc00c6ee66e6f688b77
sungmun <tjdans174@gmail.com>
2019-05-10 02:28:05 +0900

    Fead : Edite socialBox component
    기존의 socialBox컴포넌트는 버튼으로부터 유저의 profile을 받아 로그인 프로세스를 했으나, redux-thunk를 사용함에 따라 변경이 이루어 졌다
    - redirect를 위한 withRoute 제거
    - history.push를 위한 propType설정 제거
    - Login function를 위한 propType설정 제거
    - login을 위한 login function제거
    - GoogleButton,FacebookButton component에 params로 넘기는 action값 제거

--- client-side/src/component/socialBox/index.jsx
+++ client-side/src/component/socialBox/index.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { func, shape } from 'prop-types';
-import { connect } from 'react-redux';
+
 import GoogleButton from 'component/Button/Google/index';
 import FacebookButton from 'component/Button/Facebook/index';
-import loginAction from 'event/login';
-import { setToken } from 'actions/token';
+
 import './app.scss';
 
-const socialBox = ({ Login, history }) => {
-  const login = async user => {
-    try {
-      const [token, path] = await loginAction(user);
-
-      if (token) Login(token);
-      history.push(path);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
+const socialBox = () => {
   return (
     <div className="SocialLoginBox">
-      <GoogleButton action={login} />
-      <FacebookButton action={login} />
+      <GoogleButton />
+      <FacebookButton />
     </div>
   );
 };
-socialBox.propTypes = {
-  Login: func.isRequired,
-  history: shape({ push: func.isRequired }).isRequired,
-};
 
-export default connect(
-  null,
-  dispatch => ({
-    Login: token => dispatch(setToken(token)),
-  }),
-)(withRouter(socialBox));
+export default socialBox;
