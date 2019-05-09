c64a5df30a9946d6048f277551274d64910a445f
sungmun <tjdans174@gmail.com>
2019-05-09 17:15:50 +0900

    Fead : change path and edite file
    현 사용자의 로그인 token을 저장하는 reducer이며, 인자는 error,status,data 로 이루어져 있다
    - 데이터가 들어오면, error이 null값이 되고, data에 값이 들어가며, status값이 true값이 된다. (token값은 localStorage에 저장이 된다)
    - 데이터가 들어오는데 실패를 하면, error값이 지정이 된다

---
+++ client-side/src/reducer/token.js
@@ -0,0 +1,24 @@
+import { SET_TOKEN_SUCCESS } from 'actions/token';
+import { SET_TOKEN_FAILURE } from '../actions/token';
+
+const initstate = {
+  error: null,
+  status: false,
+  data: window.localStorage.getItem('token'),
+};
+
+export default (state = initstate, action) => {
+  switch (action.type) {
+    case SET_TOKEN_SUCCESS: {
+      window.localStorage.setItem('token', action.token);
+
+      return { ...state, status: true, data: action.token };
+    }
+    case SET_TOKEN_FAILURE: {
+      window.localStorage.removeItem('token');
+      return { ...state, statue: false, error: action.message };
+    }
+    default:
+      return state;
+  }
+};
