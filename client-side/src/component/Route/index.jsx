4e5ca0e986100f0a750ec7c55f5406c50fc22e25
sungmun <tjdans174@gmail.com>
2019-05-16 21:56:20 +0900

    Fead : add router
    - /talkRoomList : 기존의 talkRoom 라우터와 바뀌었다
    - /talkRoom : talkRoom의 id값을 인자값으로 넘기며, 톡방에 들어가는 페이지이다
    - /userList :친구추가를 위해 만든 페이지로 현재 자신과 친구를 재외한 모든 유저를 보여주는 페이지이다.

--- client-side/src/component/Route/index.jsx
+++ client-side/src/component/Route/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import App from 'page/App';
 import Login from 'page/Login';
+import TalkRoomList from 'page/TalkRoomList';
 import TalkRoom from 'page/TalkRoom';
+import UserList from 'page/UserList';
 
 export default () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={App} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/talkRoom" component={TalkRoom} />
+      <Route path="/talkRoomList" component={TalkRoomList} />
+      <Route path="/talkRoom" component={TalkRoom} />
+      <Route exact path="/userList" component={UserList} />
     </div>
   </BrowserRouter>
 );
