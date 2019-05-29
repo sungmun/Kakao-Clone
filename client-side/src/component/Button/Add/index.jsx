935d3bd370a34fbe0bac1a33bea2297848e30cd7
sungmun <tjdans174@gmail.com>
2019-05-29 20:41:29 +0900

    [#26] Fead : add FontAwsome icon
    FontAwsome 을 이용하여 + icon추가한다

--- client-side/src/component/Button/Add/index.jsx
+++ client-side/src/component/Button/Add/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './app.scss';
 import { Link } from 'react-router-dom';
 import { string } from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const addButton = ({ url }) => {
   return (
     <Link to={url}>
-      <div className="addButton" />
+      <div className="AddButton">
+        <FontAwesomeIcon icon={faPlus} size="2x" className="PlusIcon" />
+      </div>
     </Link>
   );
 };
