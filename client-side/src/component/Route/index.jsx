89eec2736d45e75a02b8ddabc5c1a24605baa285
sungmun <tjdans174@gmail.com>
2019-05-29 17:55:32 +0900

    [#25] Fead: change div tag to Init component
    div tag를 Init component로 변경해준다

--- client-side/src/component/Route/index.jsx
+++ client-side/src/component/Route/index.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import Init from 'layout/Init';
 import App from 'page/App';
 import Login from 'page/Login';
-import TalkRoomList from 'page/TalkRoomList';
 import TalkRoom from 'page/TalkRoom';
+import TalkRoomList from 'page/TalkRoomList';
 import UserList from 'page/UserList';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 export default () => (
   <BrowserRouter>
-    <div>
-      <Route exact path="/" component={App} />
+    <Init>
       <Route exact path="/login" component={Login} />
+      <Route exact path="/" component={App} />
       <Route path="/talkRoomList" component={TalkRoomList} />
       <Route path="/talkRoom" component={TalkRoom} />
       <Route exact path="/userList" component={UserList} />
-    </div>
+    </Init>
   </BrowserRouter>
 );
