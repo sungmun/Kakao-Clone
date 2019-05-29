14a4156de83757a82e35ab6a92cef34a42da5277
sungmun <tjdans174@gmail.com>
2019-05-29 18:14:51 +0900

    [#25] Fead: remove unnecessary parts
    기존의 부분을 제거하였다

--- client-side/src/component/FriendList/index.jsx
+++ client-side/src/component/FriendList/index.jsx
@@ -1,18 +1,9 @@
-import { listFriend } from 'actions/friend';
 import Profile from 'component/Profile/index';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const FriendList = () => {
-  const dispatch = useDispatch();
   const friend = useSelector(state => state.friendList);
-
-  useEffect(() => {
-    if (!friend.status) dispatch(listFriend());
-  }, []);
-
-  if (friend.error !== null) return <Redirect to="/login" />;
 
   return (
     <div>
