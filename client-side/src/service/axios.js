1b1f05a63a62fcf40da75eee633504ead3cd5458
sungmun <tjdans174@gmail.com>
2019-06-01 19:52:15 +0900

    [#35] Fix : edite Axios bug
    기존의 방식에서 변경을 하며, data만 반환하는것이 아닌 다른 값까지 같이 반환한다

--- client-side/src/service/axios.js
+++ client-side/src/service/axios.js
@@ -1,11 +1,13 @@
 import Axios from 'axios';
 
 export default async ({ method, url }, { token, data }) => {
-  const res = await Axios[method](`http://localhost:5000${url}`, {
-    ...data,
+  const res = await Axios({
+    method,
+    url: `http://localhost:5000${url}`,
+    data,
     headers: {
       'x-access-token': token,
     },
   });
-  return res.data;
+  return res;
 };
