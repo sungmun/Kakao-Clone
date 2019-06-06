972863bac60ce4f9a9a471576334f5d1fc7ba050
sungmun <tjdans174@gmail.com>
2019-06-06 16:25:15 +0900

    [#46] Feat : add class name
    기존의 Base 컴포넌트에 높이를 100%주기 위해 container클래스를 주었다.
    
    (그러나 차후에 첫글자가 대문자로 되있는 Container클래스의 명을 바꾸어 줘야된다)

--- client-side/src/layout/Base/index.jsx
+++ client-side/src/layout/Base/index.jsx
@@ -3,7 +3,9 @@ import { arrayOf, oneOfType, node } from 'prop-types';
 
 import './app.scss';
 
-const Base = ({ children }) => <div className="Container">{children}</div>;
+const Base = ({ children }) => (
+  <div className="Container container">{children}</div>
+);
 
 Base.propTypes = {
   children: oneOfType([arrayOf(node), node]).isRequired,
