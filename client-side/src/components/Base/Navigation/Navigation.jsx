836560b4300df70a13eac747baff703e813f0f11
sungmun <tjdans174@gmail.com>
2019-06-12 19:46:32 +0900

    [#53] Fead : change folder Name and change fileName
    component->components
    기존에는 주요 내용을 index파일에 넣어놨지만, 파일의 검색을 용이하게 하기 위해, 파일의 이름을 알이보기 쉽게 하고, index파일을 통해 불러오기 쉽게 한다
    기존에는 Link컴포넌트를 이용해 redirect를 했지만, NavLink컴포넌트를 이용함으로서 페이지의 이동에 따른 컴포넌트의 변경이 가능하게 해주었다
    Icon의 css positoin의 변경과clip속성을 추가함으로 NavLink를 통한 class변경에 따른 이벤트를 추가해주었다. 이에 따라 현제 페이지에 맞게 이미지의 색이 하얀색의 이미지가 보이게 변경이 되었다
    .

--- client-side/src/components/Base/Navigation/Navigation.jsx
+++ client-side/src/components/Base/Navigation/Navigation.jsx
@@ -1,35 +1,31 @@
-import Logo from 'component/Logo';
 import talkRoom from 'image/navi_btn_chat.png';
 import friend from 'image/navi_btn_friend.png';
 import setting from 'image/navi_btn_more.png';
 import { node, string } from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './app.scss';
 
-const NavLink = ({ url, image }) => {
+const LinkImage = ({ url, image }) => {
   return (
-    <Link to={url}>
+    <NavLink exact to={url} activeClassName="active">
       <div className="IconBox">
         <img className="Icon" src={image} alt={image} />
       </div>
-    </Link>
+    </NavLink>
   );
 };
 
-NavLink.propTypes = {
+LinkImage.propTypes = {
   url: string.isRequired,
   image: node.isRequired,
 };
 
 const Nav = () => (
-  <div>
-    <Logo />
-    <div className="Nav">
-      <NavLink url="/" image={friend} />
-      <NavLink url="/talkRoomList" image={talkRoom} />
-      <NavLink url="/setting" image={setting} />
-    </div>
+  <div className="Nav">
+    <LinkImage url="/" image={friend} />
+    <LinkImage url="/talkRoomList" image={talkRoom} />
+    <LinkImage url="/setting" image={setting} />
   </div>
 );
 
