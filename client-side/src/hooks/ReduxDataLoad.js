c9e53c54d0bbd5d16d9c38ffc728e97d552a6c16
sungmun <tjdans174@gmail.com>
2019-06-15 23:03:55 +0900

    [#53] Fead : add InitSetting Container,ReduxDataLoad,ReduxErrorCheck

---
+++ client-side/src/hooks/ReduxDataLoad.js
@@ -0,0 +1,24 @@
+import { list as listFriend } from 'actions/friend/list';
+import { getProfile } from 'actions/profile';
+import { listTalkroom } from 'actions/talkRoom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const ReduxDataLoad = ({ pathname }) => {
+  const { profile, talkRoomList, friendList } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const dispatchHooks = ({ status }, event) => {
+    if (!status) dispatch(event());
+  };
+
+  useEffect(() => {
+    if (pathname !== '/login') {
+      dispatchHooks(profile, getProfile);
+      dispatchHooks(talkRoomList, listTalkroom);
+      dispatchHooks(friendList, listFriend);
+    }
+  }, [pathname]);
+};
+
+export default ReduxDataLoad;
