8b9bacf83dabc5a9b2d797085cac1a3f58e77cde
sungmun <tjdans174@gmail.com>
2019-06-16 20:21:59 +0900

    [#53] Fead : change TalkRoom page

--- client-side/src/pages/TalkRoom/TalkRoomPage.jsx
+++ client-side/src/pages/TalkRoom/TalkRoomPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { shape, number } from 'prop-types';
 import { Redirect } from 'react-router-dom';
-import Layout from 'layout/TalkRoom';
+import TalkRoomLayout from 'containers/TalkRoom/TalkRoomContainer';
 
 const TalkRoom = ({ location }) => {
-  if (location.state.id === undefined) return <Redirect to="/" />;
+  const { id } = location.state;
+  if (id === undefined) return <Redirect to="/" />;
 
-  return <Layout id={location.state.id}>{location.state.id}</Layout>;
+  return <TalkRoomLayout id={id}>{id}</TalkRoomLayout>;
 };
 
 TalkRoom.propTypes = {
