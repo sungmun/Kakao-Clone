fe08696de4fdccaaa0cb6bd46143bbb536d386ee
sungmun <tjdans174@gmail.com>
2019-06-18 18:31:37 +0900

    [#55] Refactor : add li tag
    Item컴포넌트가 li태그가 아니므로 li태그를 추가해준다

--- client-side/src/components/Main/Profile/Profile.jsx
+++ client-side/src/components/Main/Profile/Profile.jsx
@@ -5,11 +5,13 @@ import React, { memo } from 'react';
 const Profile = ({ user, Click, Index }) => {
   const { photos, nickName, id } = user;
   return (
-    <Item
-      image={new Array(photos)}
-      LinkOp={{ url: '/user', id }}
-      contentOp={{ TabIndex: Index, Name: nickName, Event: Click }}
-    />
+    <li>
+      <Item
+        image={new Array(photos)}
+        LinkOp={{ url: '/user', id }}
+        contentOp={{ TabIndex: Index, Name: nickName, Event: Click }}
+      />
+    </li>
   );
 };
 
