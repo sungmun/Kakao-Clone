5468f5a3426f4db4df80d915cbb465767d6c8e45
sungmun <tjdans174@gmail.com>
2019-06-20 21:12:29 +0900

    [#49] Feat : add TalkList Component
    TalkList를 prop값으로 받아서 리스트를 만드는 컴포넌트로, TalkList는 배열이므로 map을 이용하여 리스트를 만든다

---
+++ client-side/src/components/TalkRoom/TalkList/TalkList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { arrayOf, shape, number, string, bool } from 'prop-types';
+import Talk from 'components/TalkRoom/Talk';
+
+const TalkListComponent = ({ talkList }) => {
+  return (
+    <ul>
+      {talkList.map(talk => {
+        return (
+          <li className="TalkList">
+            <Talk id={talk.id} message={talk.message} isUser={talk.isUser} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+TalkListComponent.propTypes = {
+  talkList: arrayOf(
+    shape({
+      id: number,
+      message: string,
+      isUser: bool,
+    }),
+  ).isRequired,
+};
+
+export default TalkListComponent;
