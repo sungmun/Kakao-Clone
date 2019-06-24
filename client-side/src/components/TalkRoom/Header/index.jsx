1645c9872699cad73dcedbe37e9c06a39db2ee58
sungmun <tjdans174@gmail.com>
2019-06-24 16:31:13 +0900

    [#47] Feat : add Header component index jsx file
    기존의 index파일의 역활을 header파일로 이동을 하며, index파일은 파일연결만 하도록 수정을 해준다

--- client-side/src/components/TalkRoom/Header/index.jsx
+++ client-side/src/components/TalkRoom/Header/index.jsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { string, shape, arrayOf } from 'prop-types';
-import UserListItem from 'components/Base/UserListItem';
-import './app.scss';
-
-const header = ({ userlist }) => {
-  return (
-    <header>
-      <UserListItem userList={userlist} Index={0} LinkOp={{ url: '#' }} />
-    </header>
-  );
-};
-
-header.propTypes = {
-  userlist: arrayOf(shape({ image: string, nickName: string })).isRequired,
-};
-export default header;
+export { default } from './header';
