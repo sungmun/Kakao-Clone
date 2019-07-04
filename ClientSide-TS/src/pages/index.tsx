8ecadf56ff1f61743772fdd27db7b3eec5d35778
sungmun <tjdans174@gmail.com>
2019-07-04 18:29:14 +0900

    [#64] Feat : convert pages index

---
+++ ClientSide-TS/src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { LoginPage as Login } from './Login';
+export { MainPage as Main } from './Main';
+export { TalkRoomPage as TalkRoom } from './TalkRoom';
+export { TalkRoomListPage as TalkRoomList } from './TalkRoomList';
+export { UserlistPage as UserList } from './UserList';
