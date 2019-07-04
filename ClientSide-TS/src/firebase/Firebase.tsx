a728a3ce5051ffd59f0044e80151d4bad8664a8b
sungmun <tjdans174@gmail.com>
2019-07-04 19:38:33 +0900

    [#57] Feat : firbase hosting init setting

---
+++ ClientSide-TS/src/firebase/Firebase.tsx
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase/app';
+
+export const app = firebase.initializeApp({
+  apiKey: 'AIzaSyB7BJT2TgddsFpXPhYu35r-YyvUzQ9QYck',
+  authDomain: 'kakao-talk-hosting.firebaseapp.com',
+  databaseURL: 'https://kakao-talk-hosting.firebaseio.com',
+  projectId: 'kakao-talk-hosting',
+  storageBucket: '',
+  messagingSenderId: '38389537690',
+  appId: '1:38389537690:web:8dba65f70bad4565',
+});
