9363514daa8ad1a3d1d463acacedef6185d36563
sungmun <tjdans174@gmail.com>
2019-07-05 16:52:14 +0900

    [#58] Feat : add firebase module
    auth를 사용하기 위해 auth를 import해주며, google과 facebook의 Provider를 추가해준다

--- ClientSide-TS/src/firebase/Firebase.tsx
+++ ClientSide-TS/src/firebase/Firebase.tsx
@@ -1,5 +1,6 @@
+import { FacebookAuthProvider_Instance, GoogleAuthProvider_Instance } from '@firebase/auth-types';
 import * as firebase from 'firebase/app';
-
+import 'firebase/auth'
 export const app = firebase.initializeApp({
   apiKey: 'AIzaSyB7BJT2TgddsFpXPhYu35r-YyvUzQ9QYck',
   authDomain: 'kakao-talk-hosting.firebaseapp.com',
@@ -9,3 +10,10 @@ export const app = firebase.initializeApp({
   messagingSenderId: '38389537690',
   appId: '1:38389537690:web:8dba65f70bad4565',
 });
+
+const { GoogleAuthProvider, FacebookAuthProvider } = firebase.auth
+
+export const googleProvider = new GoogleAuthProvider()
+export const facebookProvider = new FacebookAuthProvider()
+
+export type providerType = GoogleAuthProvider_Instance | FacebookAuthProvider_Instance
