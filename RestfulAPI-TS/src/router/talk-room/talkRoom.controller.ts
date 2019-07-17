667fc4d9c093c2ab86aee4c7096c8dd7381f7a72
sungmun <tjdans174@gmail.com>
2019-07-17 16:03:24 +0900

    [#75] Fead : convert TalkRoom controller
    - 기존의 메인 Logic은 service로 이동했으며, requset에서 body 데이터를 service로 넘기고 반환되는 데이터를 respons로 넘기는 부분을 담당한다.
    - 상속받는 Controller는 모든 Controller에서 필요한 Service를 제네릭으로 받아 클래스변수로 선언해주며, async/await형의 Error처리를 담당해주는 asyncWarp함수까지 담당해준다.
    - asyncWarp함수는 Error가 발생시 next의 인자값으로 넘겨준다

---
+++ RestfulAPI-TS/src/router/talk-room/talkRoom.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { TalkRoomService } from '../../service/TalkRooom.service';
+import { Controller } from '../controller';
+
+export class TalkRoomController extends Controller<TalkRoomService> {
+  constructor() {
+    super(new TalkRoomService());
+
+    this.router.post('/', this.asyncWarp(this.save)); // 생성
+    this.router.post('/user', this.asyncWarp(this.addUser)); // 유저추가
+
+    this.router.get('/', this.asyncWarp(this.listRead)); // 목록
+    this.router.get('/:id', this.asyncWarp(this.read)); // 내용
+
+    this.router.delete('/:id', this.asyncWarp(this.remove)); // 유저나가기
+  }
+
+  public save = async (req: Request, res: Response) => {
+    const talkRoom = await this.service.save(
+      req.body.profile,
+      req.body.friends,
+    );
+    res.status(201).send({ talkRoom });
+  };
+
+  public read = async (req: Request, res: Response) => {
+    const resData = await this.service.read(req.params.id);
+
+    res.status(200).json(resData);
+  };
+
+  public listRead = async (req: Request, res: Response) => {
+    const talkRoomList = await this.service.listRead(req.body.profile);
+    res.status(200).json({ talkRoomList });
+  };
+
+  public remove = async (req: Request, res: Response) => {
+    const removeRow = await this.service.exit(req.body.profile, req.params.id);
+    if (removeRow === 1) res.status(204).send();
+    else res.status(208).send();
+  };
+
+  public addUser = async (req: Request, res: Response) => {
+    const friend = await this.service.addUser(
+      req.body.talkroom,
+      req.body.friend,
+    );
+
+    res.status(201).json(friend);
+  };
+}
