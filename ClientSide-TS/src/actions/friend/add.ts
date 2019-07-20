581f7feddca8a98f0dfd8e80cb472e48e42307f9
sungmun <tjdans174@gmail.com>
2019-07-20 18:20:50 +0900

    [#79]  Feat : change friend type

--- ClientSide-TS/src/actions/friend/add.ts
+++ ClientSide-TS/src/actions/friend/add.ts
@@ -1,28 +1,27 @@
 import { Dispatch } from 'redux';
 import { profileServiceInstance } from 'service/profile';
-import { IUser } from 'src/interface/user.interface';
 import { IState } from 'src/reducer';
 import { failure } from './../module';
 
 export const FRIEND_DATA = 'FRIEND/ADD';
 
 export interface IFriendData {
-  friend: IUser;
+  friend: number;
   type: typeof FRIEND_DATA;
 }
 
-export const addSuccess = (friend: IUser) => ({
+export const addSuccess = (friend: number) => ({
   friend,
   type: FRIEND_DATA,
 });
 
-export const add = (user: IUser) => async (
+export const add = (user: number) => async (
   dispatch: Dispatch,
   getState: () => IState,
 ) => {
   try {
     const { token } = getState();
-    await profileServiceInstance.addFreind(token.data, user.id);
+    await profileServiceInstance.addFreind(token.data, user);
     dispatch(addSuccess(user));
   } catch (error) {
     dispatch(failure(error));
