e4b8808080a96a50bffaaf71a47a6358b75cd24f
sungmun <tjdans174@gmail.com>
2019-07-22 16:54:09 +0900

    [#68] Faet : edite talkRoom container

--- ClientSide-TS/src/containers/TalkRoom/TalkRoomContainer.tsx
+++ ClientSide-TS/src/containers/TalkRoom/TalkRoomContainer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { Header } from 'src/components/TalkRoom/Header';
+import { TalkListComponent } from 'src/components/TalkRoom/TalkList';
+import { talkLoad } from 'src/hooks/TalkLoad';
 import { IState } from 'src/reducer';
 import { TalkRoomFooterContainer } from './TalkRoomFooterContainer';
 
@@ -9,18 +11,25 @@ interface IProps {
 }
 
 const talkRoomContainer: React.SFC<IProps> = ({ id, children }) => {
-  const { data } = useSelector(({ talkRoomList }: IState) => talkRoomList);
-  const findData = data.find(talkroom => talkroom.id === id);
+  const { talkRoomList, token } = useSelector((state: IState) => state);
+  const findData = talkRoomList.data.find(talkroom => talkroom.id === id);
+  const talkList = talkLoad(id, token.data)
 
-  if (findData === undefined) return <div>Loading...</div>;
+  if (!findData || !talkList) return <div>Loading...</div>;
+
+  const sectionStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: "column"
+  }
 
   return (
-    <div className="layout container">
+    <section className="layout container" style={sectionStyle}>
       <Header userlist={findData.userList} />
-      <div className="Main">{children}</div>
+      <TalkListComponent talkList={talkList} />
       <TalkRoomFooterContainer />
-    </div>
+    </section>
   );
 };
 
 export { talkRoomContainer as TalkRoomContainer };
+
