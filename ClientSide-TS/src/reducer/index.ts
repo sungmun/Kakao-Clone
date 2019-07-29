50ccb89b477e7fe13eed7f1c2d6d5f5fa6dae52f
sungmun <tjdans174@gmail.com>
2019-07-29 16:07:47 +0900

    [#84] Refactor : refactoring profile reducer and action

--- ClientSide-TS/src/reducer/index.ts
+++ ClientSide-TS/src/reducer/index.ts
@@ -1,22 +1,23 @@
 import { combineReducers } from 'redux';
-import profile, { IProfileState } from './profile';
 import talkRoom, { ITalkroomState } from './talkRoom';
 import token, { ITokenState } from './token';
 import friend, { FriendState } from './friend';
 import header, { HeaderState } from './header';
+import profile, { ProfileState } from './profile';
 
 export interface IState {
   token: ITokenState;
   profile: IProfileState;
   talkRoomList: ITalkroomState;
   header: HeaderState;
+  profile: ProfileState;
   friendList: FriendState;
 }
 
 export default combineReducers<IState>({
   token,
-  profile,
   header,
+  profile,
   talkRoomList: talkRoom,
   friendList: friend,
 });
