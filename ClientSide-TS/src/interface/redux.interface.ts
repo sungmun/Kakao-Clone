b7f6a3c8c1786856a487595db8f638f8aacb0440
sungmun <tjdans174@gmail.com>
2019-07-29 16:04:41 +0900

    [#84] Feat : add IAsyncThunk interface
    redux에서 thunk의 async/await 함수를 위한 인터페이스

--- ClientSide-TS/src/interface/redux.interface.ts
+++ ClientSide-TS/src/interface/redux.interface.ts
@@ -1,5 +1,12 @@
-export interface IState<T> {
-  error?: Error;
-  status: boolean;
+import { ThunkAction } from 'redux-thunk';
+import { IState } from 'src/reducer/index';
+export interface IBaseState<T> extends IBase {
   data: T;
 }
+
+export interface IBase {
+  error: Error;
+  status: boolean;
+}
+export interface IAsyncThunk
+  extends ThunkAction<Promise<void>, IState, {}, any> {}
