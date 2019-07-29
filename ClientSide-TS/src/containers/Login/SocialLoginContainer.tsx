faed04caa306c4c785ddc0bee24d7f2900560898
sungmun <tjdans174@gmail.com>
2019-07-29 16:10:30 +0900

    [#84] Refactor : refactoring Login/SocialLogin Container

--- ClientSide-TS/src/containers/Login/SocialLoginContainer.tsx
+++ ClientSide-TS/src/containers/Login/SocialLoginContainer.tsx
@@ -1,20 +1,21 @@
-import { setToken } from 'actions/token';
+
 import { SocialBox } from 'components/Login/SocialLogin';
 import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { loginProcess } from 'src/containers/Login/loginProcess';
 import { app, providerType } from 'src/firebase/Firebase';
+import { asyncToken } from 'src/reducer/token';
 
 const socialContainer: React.SFC = () => {
-  const dispatch = useDispatch();
-
+  const dispatch = useDispatch()
   const onClick = async (provider: providerType) => {
     const sign = await app.auth().signInWithPopup(provider)
     const user = loginProcess(sign);
-    dispatch(setToken(user));
+    dispatch(asyncToken(user))
   }
 
   return <SocialBox LoginEvent={onClick} />
 };
 
 export { socialContainer as SocialContainer };
+
