6ba603d779220fb6a3086660ee723b33f8add874
sungmun <tjdans174@gmail.com>
2019-07-29 16:08:29 +0900

    [#84] Refactor : refactoring talkroom reducer and action

--- ClientSide-TS/src/reducer/index.ts
+++ ClientSide-TS/src/reducer/index.ts
@@ -1,17 +1,16 @@
 import { combineReducers } from 'redux';
-import talkRoom, { ITalkroomState } from './talkRoom';
 import token, { ITokenState } from './token';
 import friend, { FriendState } from './friend';
 import header, { HeaderState } from './header';
 import profile, { ProfileState } from './profile';
+import talkRoom, { TalkRoomState } from './talkRoom';
 
 export interface IState {
   token: ITokenState;
-  profile: IProfileState;
-  talkRoomList: ITalkroomState;
   header: HeaderState;
   profile: ProfileState;
   friendList: FriendState;
+  talkRoomList: TalkRoomState;
 }
 
 export default combineReducers<IState>({
