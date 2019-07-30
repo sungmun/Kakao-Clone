2bcd29f8128f0ec87ba1a3fd7f2e0d3bb9459087
sungmun <tjdans174@gmail.com>
2019-07-30 14:05:52 +0900

    [#84] Refactor : refactoring pages/UserList/UserListPage page component

--- ClientSide-TS/src/pages/UserList/UserListPage.tsx
+++ ClientSide-TS/src/pages/UserList/UserListPage.tsx
@@ -1,15 +1,15 @@
-import { SLIM } from 'actions/header';
 import { UserListContainer } from 'containers/UserList/UserListContainer';
 import { setHeaderModeHook } from 'hooks/setHeaderMode';
 import { userFilter } from 'hooks/UserListFilter';
 import { userlistHook } from 'hooks/UserListLoad';
 import * as React from 'react';
+import { HeaderMode } from 'src/reducer/header';
 import './app.css';
 
 const userlistPage: React.SFC = () => {
   const filterList = userFilter(userlistHook());
 
-  setHeaderModeHook(SLIM);
+  setHeaderModeHook(HeaderMode.SLIM);
 
   return (
     <ul className="UserListPage">
@@ -19,3 +19,4 @@ const userlistPage: React.SFC = () => {
 };
 
 export { userlistPage as UserlistPage };
+
