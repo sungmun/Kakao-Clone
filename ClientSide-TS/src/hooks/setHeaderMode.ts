e480ff9660d5f6ec4234025594724f51ed9107f0
sungmun <tjdans174@gmail.com>
2019-07-30 13:54:39 +0900

    [#84] Refactor : refactoring hooks/setHeaderMode hook

--- ClientSide-TS/src/hooks/setHeaderMode.ts
+++ ClientSide-TS/src/hooks/setHeaderMode.ts
@@ -1,11 +1,23 @@
-import { BASE, Mode, setMode } from 'actions/header';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { headActions, HeaderMode } from 'src/reducer/header';
 
-export const setHeaderModeHook = (MODE_TYPE: Mode) => {
+export const setHeaderModeHook = (MODE_TYPE: HeaderMode) => {
   const dispatch = useDispatch();
   useEffect((): (() => void) => {
-    dispatch(setMode(MODE_TYPE));
-    return () => dispatch(setMode(BASE));
+    switch (MODE_TYPE) {
+      case HeaderMode.BASE:
+        dispatch(headActions.setBaseMode());
+        break;
+      case HeaderMode.NONE:
+        dispatch(headActions.setNoneMode());
+        break;
+      case HeaderMode.SLIM:
+        dispatch(headActions.setSlimMode());
+        break;
+    }
+    return () => {
+      dispatch(headActions.setBaseMode());
+    };
   }, []);
 };
